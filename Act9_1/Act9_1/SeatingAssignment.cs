55a642ff311ef019c7bd3a8473ad8022f00c3d26
Nemanja <nemadjo94@gmail.com>
2019-07-09 10:10:39 +0200

    Add files via upload

---
+++ Act9_1/Act9_1/SeatingAssignment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act9_1
+{
+    class SeatingAssignment
+    {
+        private int row;
+        private int seat;
+        private string student;
+        
+        public int Row
+        {
+            get { return row; }
+            set { row = value; }
+        }
+
+        public int Seat
+        {
+            get { return seat; }
+            set { seat = value; }
+        }
+
+        public string Student
+        {
+            get { return student; }
+            set { student = value; }
+        }
+        
+        //Constructor
+        public SeatingAssignment(int row, int seat, string student)
+        {
+            this.Row = row;
+            this.Seat = seat;
+            this.Student = student;
+        }
+
+    }
+}
